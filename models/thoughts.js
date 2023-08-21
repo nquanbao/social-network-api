91edd80414fddaca3f91f608dec169c0a20496f7
Bao Nguyen <bnguyen88@knights.ucf.edu>
2023-08-21 11:38:59 -0400

    created files for models

---
+++ models/thoughts.js
@@ -0,0 +1,39 @@
+const { Schema, model} = require('mongoose');
+const reactions = require('./reactions')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            require: true,
+            minlength: 1,
+            maxlength: 128,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactions]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+    .virtuals('reactionCount')
+    .get(function(){
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
